edead77ea4c0622aa5e7d6f6aacf3728fd43eed4
pistonpandu-dev <pistonpandu@gmail.com>
2026-06-23 20:02:35 +0700

    Create index.ts

---
+++ src/modules/locations/index.ts
@@ -0,0 +1,5 @@
+export * from './location.controller';
+export * from './location.service';
+export * from './location.repository';
+export * from './location.validator';
+export { default as locationRoutes } from './location.routes';
