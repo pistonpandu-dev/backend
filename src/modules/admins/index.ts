b163ecb574e6f31d62d0b72076999696196883bc
pistonpandu-dev <pistonpandu@gmail.com>
2026-06-23 19:25:01 +0700

    Create index.ts

---
+++ src/modules/admins/index.ts
@@ -0,0 +1,5 @@
+export * from './admin.controller';
+export * from './admin.service';
+export * from './admin.repository';
+export * './admin.validator';
+export { default as adminRoutes } from './admin.routes';
