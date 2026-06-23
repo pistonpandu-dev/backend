a10c6f9f322a4b00a3f2d1ecbb5c7dd922e6b467
pistonpandu-dev <pistonpandu@gmail.com>
2026-06-23 20:29:22 +0700

    Create index.ts

---
+++ src/modules/files/index.ts
@@ -0,0 +1,5 @@
+export * from './file.controller';
+export * from './file.service';
+export * from './file.repository';
+export * from './file.validator';
+export { default as fileRoutes } from './file.routes';
