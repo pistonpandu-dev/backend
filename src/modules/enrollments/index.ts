9c6174ee985baa36bf9e57ab28e46846215c4762
pistonpandu-dev <pistonpandu@gmail.com>
2026-06-23 19:44:54 +0700

    Create index.ts

---
+++ src/modules/enrollments/index.ts
@@ -0,0 +1,5 @@
+export * from './enrollment.controller';
+export * from './enrollment.service';
+export * from './enrollment.repository';
+export * from './enrollment.validator';
+export { default as enrollmentRoutes } from './enrollment.routes';
