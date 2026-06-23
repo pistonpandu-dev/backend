d13e49cd6146192ec9b6cbd61187dbec3f6fb783
pistonpandu-dev <pistonpandu@gmail.com>
2026-06-23 20:36:38 +0700

    Create index.ts

---
+++ src/modules/dashboard/index.ts
@@ -0,0 +1,4 @@
+export * from './dashboard.controller';
+export * from './dashboard.service';
+export * from './dashboard.repository';
+export { default as dashboardRoutes } from './dashboard.routes';
