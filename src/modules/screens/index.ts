3a3d4735e9813314a43e21839bbdfcd37386670f
pistonpandu-dev <pistonpandu@gmail.com>
2026-06-23 20:25:44 +0700

    Create index.ts

---
+++ src/modules/screens/index.ts
@@ -0,0 +1,5 @@
+export * from './screen.controller';
+export * from './screen.service';
+export * from './screen.repository';
+export * './screen.validator';
+export { default as screenRoutes } from './screen.routes';
