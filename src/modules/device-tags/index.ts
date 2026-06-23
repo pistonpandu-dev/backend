3030095fb72594fdcabcc6f18c1d796fe00046b5
pistonpandu-dev <pistonpandu@gmail.com>
2026-06-23 19:42:03 +0700

    Create index.ts

---
+++ src/modules/device-tags/index.ts
@@ -0,0 +1,5 @@
+export * from './tag.controller';
+export * from './tag.service';
+export * from './tag.repository';
+export * from './tag.validator';
+export { default as tagRoutes } from './tag.routes';
