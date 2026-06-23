d57b83bd11dedfcce1064bed34655b9fd60b12db
pistonpandu-dev <pistonpandu@gmail.com>
2026-06-23 19:37:34 +0700

    Create index.ts

---
+++ src/modules/device-groups/index.ts
@@ -0,0 +1,5 @@
+export * from './group.controller';
+export * from './group.service';
+export * from './group.repository';
+export * from './group.validator';
+export { default as groupRoutes } from './group.routes';
