e24c5943f1742ddef31545b6ace59a7cfecf33ca
pistonpandu-dev <pistonpandu@gmail.com>
2026-06-23 20:39:11 +0700

    Create index.ts

---
+++ src/modules/statistics/index.ts
@@ -0,0 +1,4 @@
+export * from './statistics.controller';
+export * from './statistics.service';
+export * from './statistics.repository';
+export { default as statisticsRoutes } from './statistics.routes';
