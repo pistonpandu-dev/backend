e203259d58abc5584a2d30f01ddf506bc9405919
pistonpandu-dev <pistonpandu@gmail.com>
2026-06-23 20:09:46 +0700

    Create index.ts

---
+++ src/modules/geofence/index.ts
@@ -0,0 +1,5 @@
+export * from './geofence.controller';
+export * from './geofence.service';
+export * from './geofence.repository';
+export * from './geofence.validator';
+export { default as geofenceRoutes } from './geofence.routes';
