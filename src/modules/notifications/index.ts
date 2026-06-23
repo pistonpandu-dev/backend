6972a58ab2c9253efc6e90004d46a529edeb4c64
pistonpandu-dev <pistonpandu@gmail.com>
2026-06-23 20:33:08 +0700

    Create index.ts

---
+++ src/modules/notifications/index.ts
@@ -0,0 +1,5 @@
+export * from './notification.controller';
+export * from './notification.service';
+export * './notification.repository';
+export * from './notification.validator';
+export { default as notificationRoutes } from './notification.routes';
